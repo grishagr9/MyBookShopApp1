71e5ffcff4faca05151eb8b2d4cc56e135a75472
grishagr9 <grisha-rnd10@yandex.ru>
2023-09-23 16:16:29 +0300

    new

--- src/main/java/com/example/MyBookShopApp/dao/TestEntityDao.java
+++ src/main/java/com/example/MyBookShopApp/dao/TestEntityDao.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 public class TestEntityDao extends AbstractHibernateDao<TestEntity> {
     public TestEntityDao(){
         super();
+
         setClazz(TestEntity.class);
     }
 }
